528c61a2a5a37becd5069574fe0578b877aeece1
Moonbench <34589537+moonbench@users.noreply.github.com>
2023-03-03 00:34:42 +1100

    Title page easter egg

--- Demo/Scripts/Game.gd
+++ Demo/Scripts/Game.gd
@@ -1,6 +1,24 @@
 # Runs the demo's main scene
 extends Node
 
+export var car_count = 6
+
+onready var train_vehicle_reference = load("res://Scenes/TrainVehicle.tscn")
+onready var engine = $TrainEngine
+
+func _setup_train():
+	engine.add_to_track($Tracks/Track, 400)
+	
+	var last_car = engine
+	for index in range(car_count):
+		var car = train_vehicle_reference.instance()
+		add_child(car)
+		last_car.set_follower_car(car)
+		last_car = car
+
+func _on_Timer_timeout():
+	_setup_train()
+
 func _on_Button_pressed():
 	get_tree().change_scene_to(load("res://Demo/Scenes/TestTracks1.tscn"))
 
