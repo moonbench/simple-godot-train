c8da2838f588f691bc712ec9f0eb45f40b2f528c
Moonbench <34589537+moonbench@users.noreply.github.com>
2024-03-15 21:47:41 +1100

    Whitespace cleanup

--- Demo/Scripts/TestTracks6.gd
+++ Demo/Scripts/TestTracks6.gd
@@ -6,28 +6,27 @@ func _ready() -> void:
 func _setup_train():
 	var train = load("res://Scenes/train.tscn").instantiate()
 	add_child(train)
-	
 	train.enable_player_control()
+	
 	var engine = load("res://Scenes/TrainEngine.tscn").instantiate()
 	add_child(engine)
 	engine.add_to_track($Tracks/Track, 1200)
-	var camera : Camera2D = load("res://Demo/Scenes/ChaseCamera.tscn").instantiate()
-	camera.ignore_rotation = false
-	camera.rotation = -PI/2
-	engine.add_child(camera)
 	train.add_vehicle(engine)
 	
 	var e2 = load("res://Scenes/TrainEngine.tscn").instantiate()
 	add_child(e2)
 	train.add_vehicle(e2)
-
+	
 	for _i in range(30):
 		_add_car_to_train(train)
 	
 	UI.add_driving_ui()
 	train.train_info.connect(UI.driving_ui.update_train_info)
 	
-	
+	var camera : Camera2D = load("res://Demo/Scenes/ChaseCamera.tscn").instantiate()
+	camera.ignore_rotation = false
+	camera.rotation = -PI/2
+	engine.add_child(camera)
 
 func _add_car_to_train(train: Train) -> void:
 	var car = load("res://Scenes/TrainVehicle.tscn").instantiate()
